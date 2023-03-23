ac2f15d3aef38134abd2901ed5767057797b2c24
aiiitsh <82343631+aiiitsh@users.noreply.github.com>
2023-03-23 15:12:14 +0200

    Update A_Sereja_and_Dima.cpp
    I found that the erase function that I have used in the previous code makes the time complexity o(n^2)
    I used two pointers rather than it which makes the code of time complicity o(n)

--- A_Sereja_and_Dima.cpp
+++ A_Sereja_and_Dima.cpp
@@ -1,38 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n, sum1=0, sum2=0;
-    cin>>n;
+int main() {
+    int n, sum1 = 0, sum2 = 0;
+    cin >> n;
     vector<int> vect1(n);
-    for (int i=0;i<n;i++){
-        cin>>vect1[i];
+    for (int i = 0; i < n; i++) {
+        cin >> vect1[i];
     }
-    
-    
-    for (int i=0;i<n;i++){
-        if(i%2!=0){
-            if(vect1.front()>vect1.back()){
-                sum1=sum1+vect1.front();
-                vect1.erase(vect1.begin());
+
+    int left = 0, right = n - 1;
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            if (vect1[left] > vect1[right]) {
+                sum1 += vect1[left];
+                left++;
+            } else {
+                sum1 += vect1[right];
+                right--;
             }
-            else{
-                sum1=sum1+vect1.back();
-                vect1.erase(std::prev(vect1.end()));
+        } else {
+            if (vect1[left] > vect1[right]) {
+                sum2 += vect1[left];
+                left++;
+            } else {
+                sum2 += vect1[right];
+                right--;
             }
         }
-        else if(i%2==0){
-            if(vect1.front()>vect1.back()){
-                sum2=sum2+vect1.front();
-                vect1.erase(vect1.begin());
-            }
-            else{
-                sum2=sum2+vect1.back();
-                vect1.erase(std::prev(vect1.end()));
-            }
-        }
+        if (left > right) break;
     }
-    cout<<sum2<<" "<<sum1;
+    cout << sum1 << " " << sum2;
 
     return 0;
 }
