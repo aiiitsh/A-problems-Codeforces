3bea15a2ba94582b770e0052921c1361c98ac5fd
aiiitsh <82343631+aiiitsh@users.noreply.github.com>
2023-03-26 17:33:39 +0200

    Update A_Buy_a_Shovel.cpp
    I came up with a way better approach here:
    you calculate the full price and check if the change equals r or not as you have unlimited numbers of 10 coins and one coin of value r. so if the change equals r, "i" will be the minimum number of shovels.

--- A_Buy_a_Shovel.cpp
+++ A_Buy_a_Shovel.cpp
@@ -3,30 +3,18 @@
 using namespace std;
 
 int main() {
-    int price, r, min_1 = -1, min_2 = -1, test1, test2;
-    cin >> price >> r;
-    for(int i = 1; i <= 10; i++) {
-        test1 = price * i;
-        if(test1 % 10 == 0 || (test1 - r) % 10 == 0) {
-            min_1 = i;
-            break;
+    int price, r;
+    cin>>price>>r;
+
+    for(int i=1;i<10;i++){
+        int total_price = price * i;
+        int change = total_price % 10;
+        if(change == r || change == 0){
+            cout << i << endl;
+            return 0;
         }
     }
-    for(int i = 1; i <= 10; i++) {
-        test2 = (price * i) - r;
-        if(test2 % 10 == 0 && test2 >= 0) {
-            min_2 = i;
-            break;
-        }
-    }
-    if(min_1 == -1 && min_2 == -1) {
-        cout << -1 << endl;
-    } else if(min_1 == -1) {
-        cout << min_2 << endl;
-    } else if(min_2 == -1) {
-        cout << min_1 << endl;
-    } else {
-        cout << min(min_1, min_2) << endl;
-    }
+
     return 0;
 }
+
