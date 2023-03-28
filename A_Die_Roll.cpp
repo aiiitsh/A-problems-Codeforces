3690ed96b0b9f24adc0a6961a4749d168eda657b
aiiitsh <82343631+aiiitsh@users.noreply.github.com>
2023-03-28 13:55:21 +0200

    Update A_Die_Roll.cpp

--- A_Die_Roll.cpp
+++ A_Die_Roll.cpp
@@ -9,32 +9,14 @@ typedef long long ll;
 const int nmax= 1e5 + 5;
  
 int main(){
-    int x, y;
-    cin>>x>>y;
-    int a = max(x, y);
-    switch (a){
-        case 6:
-        cout<<"1/6";
-        break;
-        case 5: 
-        cout<<"1/3";
-        break;
-        case 4:
-        cout<<"1/2";
-        break;
-        case 3:
-        cout<<"2/3";
-        break;
-        case 2:
-        cout<<"5/6";
-        break;
-        case 1:
-        cout<<"1/1";
-        break;
-
-
-    }
-
-    
+    int y, w;
+    cin >> y >> w;
+    int max_value = max(y, w);
+    int numerator = 6 - max_value + 1;
+    int denominator = 6;
+    int gcd = __gcd(numerator, denominator);
+    numerator /= gcd;
+    denominator /= gcd;
+    cout << numerator << "/" << denominator << endl;
     return 0;
 }
