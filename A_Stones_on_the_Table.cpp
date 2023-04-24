6c66fddff48d4abd9562e229e28f2e085a6cdce7
aiiitsh <82343631+aiiitsh@users.noreply.github.com>
2023-04-24 21:31:03 +0200

    Update A_Stones_on_the_Table.cpp

--- A_Stones_on_the_Table.cpp
+++ A_Stones_on_the_Table.cpp
@@ -4,13 +4,15 @@ using namespace std;
 int main() {
     int n;
     cin>>n;
-    string x;
-    cin >>x;
     int counter=0;
-    for (int i=0;i<n;i++){
-        if (x[i]==x[i+1]){
+    char c, z='q';
+    for(int i=0;i<n;i++){
+        cin>>c;
+        if(c==z){
             counter++;
         }
+        z=c;
+
     }
     cout<<counter;
 
